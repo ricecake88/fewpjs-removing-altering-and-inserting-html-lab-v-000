ef7cb01cf776bbf659907d46b06f3607add99d25
Grace Shih <vocalise@gmail.com>
2019-12-18 05:47:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+(document.querySelector("main")).remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
