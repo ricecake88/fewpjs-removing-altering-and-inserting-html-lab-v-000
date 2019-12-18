3db0aeb02eb6af7e57eeb870d8101c04ef1508e7
Grace Shih <vocalise@gmail.com>
2019-12-18 05:48:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 (document.querySelector("main")).remove();
 let newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
+//newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
 newHeader.innerHTML = "YOUR-NAME is the champion";
-document.body.appendChild(newHeader);
+//document.body.appendChild(newHeader);
